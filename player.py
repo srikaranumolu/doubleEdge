9c28195113ed3d0dff6f7395e7d3adc795f59678
Srikar A <anumolusk@gmail.com>
2024-11-04 13:49:15 -0500

    frist

--- player.py
+++ player.py
@@ -1,13 +1,12 @@
+import time
 import pygame
 import sys
 from pygame.locals import *
-from time import sleep
 
 pygame.init()
 
-WINDOW_SIZE = (0, 0)
-screen = pygame.display.set_mode(WINDOW_SIZE, pygame.FULLSCREEN)
-pygame.display.set_caption("Character Example")
+screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+pygame.display.set_caption("DoubleEdge Sword")
 
 class Player:
     def __init__(self, image, x, y, width, height):
@@ -15,34 +14,52 @@ class Player:
         self.image = pygame.transform.scale(self.image, (width, height))
         self.x = x
         self.y = y
-        self.velocity_y = 0
-        self.is_jumping = False
-
+        self.og = y
+        self.velocity_y = 7
+        self.is_jump = False
+        self.mass = 1
+        self.last_time = 0
 
     def draw(self, screen):
         screen.blit(self.image, (self.x, self.y))
 
 
-player = Player("player.png", 100, 100, 50, 50)
+
+player = Player("player.png", 100, 100, 100, 100)
 
 while True:
+    screen.fill((0, 0, 0))
+    player.draw(screen)
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == KEYDOWN:
-            if event.key == K_LEFT:
-                player.x -= 15
-            elif event.key == K_RIGHT:
-                player.x += 15
-            elif event.key == K_UP:
-                player.y += 15
-            elif event.key == K_DOWN:
-                player.y += 15
-            elif event.key == K_ESCAPE:
+            if event.key == K_ESCAPE:
                 pygame.quit()
                 sys.exit()
 
-    screen.fill((0, 0, 0))  # Clear the screen with black color
-    player.draw(screen)     # Draw the player
+    keys = pygame.key.get_pressed()
+    if keys[K_LEFT]:
+        player.x -= 5
+    if keys[K_RIGHT]:
+        player.x += 5
+    current_time = time.time()
+    if not player.is_jump and keys[K_UP] and (current_time - player.last_time) > 1:
+            player.is_jump = True
+            player.last_time = current_time
+    if player.is_jump:
+        F = (1/2) * player.mass * (player.velocity_y ** 2)
+        player.y-=F
+        player.velocity_y-=0.5
+        if player.velocity_y < 0:
+            player.mass = -1
+        if player.velocity_y == -8:
+            player.is_jump = False
+            player.velocity_y = 7
+            player.mass = 1
+            player.y = player.og
+
+    pygame.time.delay(10)
+        # Draw the player
     pygame.display.update() # Update the display
